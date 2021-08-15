3c941a6bbb043316838db06faf14172ef26fbd30
Alex Ferreras <alexferreras03@gmail.com>
2021-08-14 20:37:09 -0400

    add documentation

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -24,17 +24,12 @@ public class WeatherApiRestController {
     }
 
     /**
-     * Requirements:
-     * The REST service must expose the /weather endpoint, which allows for managing the collection of weather records in the following way:
-     *
-     * POST request to /weather:
-     *
-     * creates a new weather data record
-     * expects a valid weather data object as its body payload, except that it does not have an id property; you can assume that the given object is always valid
-     * adds the given object to the collection and assigns a unique integer id to it
-     * the response code is 201 and the response body is the created record, including its unique id
+     * {@code POST  /weather}  : Creates a new weather.
+     * <p>
+     * Creates a new weather,
+     * @param weather the valid weather to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the created weather.
      */
-
     @PostMapping
     public ResponseEntity<?> saveWeather (@Valid @RequestBody Weather weather){
         Weather createdWeather = weatherRepository.save(weather);
@@ -42,19 +37,15 @@ public class WeatherApiRestController {
     }
 
     /**
-     * GET request to /weather:
+     * {@code GET /weather} : get all weather.
      *
-     * the response code is 200
-     * the response body is an array of matching records, ordered by their ids in increasing order
-     * accepts an optional query string parameter, date, in the format YYYY-MM-DD, for example /weather/?date=2019-06-11. When this parameter is present, only the records with the matching date are returned.
-     * accepts an optional query string parameter, city, and when this parameter is present, only the records with the matching city are returned. The value of this parameter is case insensitive, so "London" and "london" are equivalent. Moreover, it might contain several values, separated by commas (e.g. city=london,Moscow), meaning that records with the city matching any of these values must be returned.
-     * accepts an optional query string parameter, sort, that can take one of two values: either "date" or "-date". If the value is "date", then the ordering is by date in ascending order. If it is "-date", then the ordering is by date in descending order. If there are two records with the same date, the one with the smaller id must come first.
+     * @param date, city, sort @optionals the pagination information.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all weathers.
      */
     @GetMapping
     ResponseEntity<List<Weather>> getWeatherList(@RequestParam(name = "date", required = false) Optional<Date> date,
                                      @RequestParam(name = "city",required = false) Optional<List<String>> city,
                                      @RequestParam(name = "sort", required = false) Optional<String> sort) {
-
 
         List<Weather> sorterWeatherList = weatherRepository.findAll();
         sorterWeatherList.sort(Comparator.comparing(Weather::getId));
@@ -87,13 +78,11 @@ public class WeatherApiRestController {
     }
 
     /**
-     * GET request to /weather/<id>:
+     * {@code GET /weather/<id>} : get the weather whith the provided id.
      *
-     * returns a record with the given id
-     * if the matching record exists, the response code is 200 and the response body is the matching object
-     * if there is no record in the collection with the given id, the response code is 404
+     * @param id of the weather to find.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the weather whith provided id, or with status {@code 404 (Not Found)}.
      */
-
     @GetMapping("/{id}")
     ResponseEntity<?> getWeatherById(@PathVariable int id) {
 
@@ -103,7 +92,5 @@ public class WeatherApiRestController {
         }
         return new ResponseEntity<>(weather, HttpStatus.OK);
     }
-
-
 
 }
