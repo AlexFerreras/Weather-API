5f6976cdc697b0906a1ba454375744fa1f29997e
Alex Ferreras <alexferreras03@gmail.com>
2021-08-17 11:32:12 -0400

    fix DateTimeFormat in getWeatherList

--- src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/hackerrank/weather/controller/WeatherApiRestController.java
@@ -3,11 +3,14 @@ package com.hackerrank.weather.controller;
 import com.hackerrank.weather.model.Weather;
 import com.hackerrank.weather.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -43,16 +46,21 @@ public class WeatherApiRestController {
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all weathers.
      */
     @GetMapping
-    ResponseEntity<List<Weather>> getWeatherList(@RequestParam(name = "date", required = false) Optional<Date> date,
-                                     @RequestParam(name = "city",required = false) Optional<List<String>> city,
-                                     @RequestParam(name = "sort", required = false) Optional<String> sort) {
+    ResponseEntity<List<Weather>> getWeatherList(@RequestParam(name = "date", required = false)
+                                                 @DateTimeFormat(pattern = "yyyy-MM-dd")Optional<Date> date,
+                                                 @RequestParam(name = "city",required = false) Optional<List<String>> city,
+                                                 @RequestParam(name = "sort", required = false) Optional<String> sort) {
 
         List<Weather> sorterWeatherList = weatherRepository.findAll();
         sorterWeatherList.sort(Comparator.comparing(Weather::getId));
 
+
+
+
+
         if (date.isPresent()){
             sorterWeatherList = sorterWeatherList.stream()
-                    .filter(weather -> weather.getDate().equals(date.get()))
+                    .filter(weather -> weather.getDate().compareTo(date.get()) == 0)
                     .collect(Collectors.toList());
         }
 
@@ -92,5 +100,34 @@ public class WeatherApiRestController {
         }
         return new ResponseEntity<>(weather, HttpStatus.OK);
     }
+
+
+    /**
+     * GET request to /repos/{repoId}/events:
+     *
+     * returns a collection of events related to the given repository
+     * the response code is 200, and the response body is an array of events related to the given repository ordered by their ids in increasing order
+
+     * GET request to /events/{eventId}:
+     * returns an event with the given id
+     * if the matching event exists, the response code is 200 and the response body is the matching event object
+     * if there is no event in the collection with the given id, the response code is 404
+     *
+     *
+     * GET request to all endpoints that return a collection of objects (/repos/{repoId}/events ,/users/{userId}/events, /events)
+     *
+     * takes the optional parameter public, which is used to filter the returned objects, e.g.:
+     */
+
+    @GetMapping("/events/{eventId}")
+    ResponseEntity<?> getEventById(@PathVariable int eventId) {
+
+        Optional<Weather> weather = weatherRepository.findById(eventId);
+        if(!weather.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(weather);
+    }
+
 
 }
