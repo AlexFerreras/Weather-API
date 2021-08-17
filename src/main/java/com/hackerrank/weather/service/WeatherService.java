482ceaad2329e72bd96b34cade16664165441b4d
Alex Ferreras <alexferreras03@gmail.com>
2021-08-17 11:56:33 -0400

    refactor FilteredWeatherList method

--- src/main/java/com/hackerrank/weather/service/WeatherService.java
+++ src/main/java/com/hackerrank/weather/service/WeatherService.java
@@ -3,9 +3,7 @@ package com.hackerrank.weather.service;
 import com.hackerrank.weather.model.Weather;
 import com.hackerrank.weather.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Comparator;
 import java.util.Date;
@@ -22,35 +20,47 @@ public class WeatherService {
         this.weatherRepository = weatherRepository;
     }
 
-    public List<Weather> getFilteredWeatherList( Optional<Date> date, Optional<List<String>> city,
+    public List<Weather> getFilteredWeatherList( Optional<Date> date, Optional<List<String>> cities,
                                                  Optional<String> sort){
 
         List<Weather> sortedWeatherList = weatherRepository.findAll();
         sortedWeatherList.sort(Comparator.comparing(Weather::getId));
 
         if (date.isPresent()){
-            sortedWeatherList = sortedWeatherList.stream()
-                    .filter(weather -> weather.getDate().compareTo(date.get()) == 0)
-                    .collect(Collectors.toList());
+            sortedWeatherList = getFilterWeatherListByDate(date.get(), sortedWeatherList);
         }
 
-        if (city.isPresent()){
-            sortedWeatherList = sortedWeatherList.stream()
-                    .filter(weather -> city.get().stream()
-                            .anyMatch(weather.getCity()::equalsIgnoreCase))
-                    .collect(Collectors.toList());
+        if (cities.isPresent()){
+            sortedWeatherList = getFilterWeatherListByCity(cities.get(), sortedWeatherList);
         }
 
         if(sort.isPresent()){
-            if(sort.get().equals("date")){
-                sortedWeatherList.sort(Comparator.comparing(Weather::getDate));
-            }
-
-            if(sort.get().equals("-date")){
-                sortedWeatherList.sort(Comparator.comparing(Weather::getDate).reversed());
-            }
+            sortedWeatherListByDate(sort.get(), sortedWeatherList);
         }
 
         return sortedWeatherList;
+    }
+
+    private void sortedWeatherListByDate(String sort, List<Weather> sortedWeatherList) {
+        if(sort.equals("date")){
+            sortedWeatherList.sort(Comparator.comparing(Weather::getDate));
+        }
+
+        if(sort.equals("-date")){
+            sortedWeatherList.sort(Comparator.comparing(Weather::getDate).reversed());
+        }
+    }
+
+    private List<Weather> getFilterWeatherListByCity(List<String> cityList, List<Weather> sortedWeatherList) {
+        return sortedWeatherList.stream()
+                .filter(weather -> cityList.stream()
+                        .anyMatch(weather.getCity()::equalsIgnoreCase))
+                .collect(Collectors.toList());
+    }
+
+    private List<Weather> getFilterWeatherListByDate(Date date, List<Weather> sortedWeatherList) {
+        return sortedWeatherList.stream()
+                .filter(weather -> weather.getDate().compareTo(date) == 0)
+                .collect(Collectors.toList());
     }
 }
